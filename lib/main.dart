e14cebe00f275324f02cc7d588c407d39931730e
juan camilo castillo <npgcamilo@gmail.com>
2020-06-03 21:55:02 -0500

    clean master

--- lib/main.dart
+++ lib/main.dart
@@ -17,38 +17,8 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-      child: Container(
-        //color: Colors.blue,
-        //constraints: BoxConstraints.expand(),
-        width: 200,
-        height: 200,
-        padding: EdgeInsets.all(8.0),
-        alignment: Alignment.center,
-        //transform: Matrix4.rotationZ(0.5),
-        decoration: BoxDecoration(
-            color: Colors.blue,
-            border: Border.all(
-                color: Colors.black, width: 2.0, style: BorderStyle.solid),
-            //borderRadius: BorderRadius.all(Radius.circular(20.0)),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey,
-                blurRadius: 4.0,
-                spreadRadius: 2.0
-              )
-            ],
-            gradient: LinearGradient(
-              begin: Alignment.centerLeft,
-              end: Alignment.centerRight,
-              colors: [
-              Colors.blue,
-              Colors.white
-            ]),
-            shape: BoxShape.circle
-          ),
-        child: Text("Flutter Essentials",
-            style: TextStyle(fontSize: 20, color: Colors.white)),
-      ),
-    ));
+          child: Text("Hello world!!"),
+        )
+      );
   }
 }
